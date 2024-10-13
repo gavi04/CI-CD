b73e75f071e8b1b8729910ddb786647e6b7aa7f1
gavy04 <gavisohal87@gmail.com>
2024-10-14 00:38:14 +0530

    code removed

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -2,11 +2,7 @@
 
 export default async function Page() {
   const session = await getServerSession(authOptions);  
-  if (session?.user) {
-    redirect('/dashboard')
-  }  
-  else {
-    redirect('/api/auth/signin')
+ 
   }
 }
 //hello
