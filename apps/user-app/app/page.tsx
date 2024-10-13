c6c8d0c167ad64a03cff22acc37b2d3246dc192a
Imanpreet singh <125171793+gavi04@users.noreply.github.com>
2024-10-14 01:07:52 +0530

    Update page.tsx

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -2,8 +2,8 @@ import { getServerSession } from "next-auth";
 import { redirect } from 'next/navigation'
 import { authOptions } from "./lib/auth";
 
+
 export default async function Page() {
-  export default async function Page() {
   const session = await getServerSession(authOptions);  
   if (session?.user) {
     redirect('/dashboard')
@@ -12,6 +12,6 @@ export default async function Page() {
     redirect('/api/auth/signin')
   }
 }
-}
+
 //hello
 //test ci pipeline
