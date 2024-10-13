a6a8c7ba5df737c970cd505a2ec13278ccaae9ee
gavy04 <gavisohal87@gmail.com>
2024-10-14 00:52:52 +0530

    removed

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -3,12 +3,6 @@ import { redirect } from 'next/navigation'
 import { authOptions } from "./lib/auth";
 
 export default async function Page() {
-  const session = await getServerSession(authOptions);  
-  if (session?.user) {
-    redirect('/dashboard')
-  }  
-  else {
-    redirect('/api/auth/signin')
-  }
+  
 }
 //hello
